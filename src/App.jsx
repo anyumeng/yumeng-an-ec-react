e6b01da6d325fc054924bd290b93b734e53e0fa1
Yumeng An <an.yum@northeastern.edu>
2023-02-26 20:38:12 -0800

    Update MA5 into a React app.

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('')
+  const [style, setStyle] = useState({
+    height: "100px",
+    width: "100px",
+    border: "1px solid black",
+  })
+  const [invalidMessage, setInvalidMessage] = useState('')
+
+  function changeColor() {
+    const validColors = new Set(['red', 'green', 'blue']);
+    if (validColors.has(color.toLocaleLowerCase())) {
+      setStyle(style => ({
+        ...style,
+        backgroundColor: color
+      }));
+    } else {
+        setInvalidMessage(color + ' is not a valid color. Please choose green, blue or red.');
+    }
+  }
+
+  function onColorInput(event) {
+    const colorValue = event.target.value;
+    setColor(colorValue);
+  }
+
+  return (
+    <div className="App">
+      <div className="box" style={style}></div>
+      <div className="input-button">
+        <input type="text" value={color} id="color" onInput={onColorInput} />
+        <button onClick={changeColor}>Change color</button>
+      </div>
+      <p id="invalid-message">{invalidMessage}</p>
+    </div>
+  )
+}
+
+export default App
